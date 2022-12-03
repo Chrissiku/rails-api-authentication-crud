377cdfe02928d073c5bcd36f26a870076cb7c51f
Chris Siku <chrissiku5@gmail.com>
2022-12-03 21:20:18 +0200

    Initial commit

---
+++ react-redux-jwt-auth/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>
+    </>
+  );
+};
+
+export default App;
