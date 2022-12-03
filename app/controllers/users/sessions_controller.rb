34ed441a8f44160cb750aee0ec55b1effdf31ef6
Chris Siku <chrissiku5@gmail.com>
2022-12-03 11:41:08 +0200

    Implement the sing_up, sign_in and sign_out actions

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,26 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  respond_to
+  
+  private
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def respond_with  (resource, options = {})
+    if resource.persisted?
+      render json: { status: {code: 200, message: 'Signed in successfully', data: resource}}
+    else
+      render json: { status: {code: 500, message: 'User could not be signed in', errors: resource.errors.full_mesages}}
+    end
+  end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload['sub'])
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    if current_user
+      render json: { status: {code: 200, message: 'Signed out successfully', data: current_user}}
+    else
+      render json: { status: {code: 401, message: 'User could not be signed out', errors: current_user.errors.full_mesages}}
+    end
+  end
 end
